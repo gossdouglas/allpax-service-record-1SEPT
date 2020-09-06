1c5b4bcf8fbd3779319cd5b6ad4975ff6c0ebf7f
gossdouglas <gossdouglas@yahoo.com>
2020-09-06 10:53:24 -0500

    got the team sections working for filtered and unfilltered.

--- Controllers/dailyReportAllController.cs
+++ Controllers/dailyReportAllController.cs
@@ -18,35 +18,31 @@ namespace allpax_service_record.Controllers
     {
         private allpaxServiceRecordEntities db = new allpaxServiceRecordEntities();
 
-        // GET: customers
         public ActionResult Index()
         {
-
             List<vm_dailyReportViewAll> dailyRptViewAlls = new List<vm_dailyReportViewAll>();
             string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
             SqlConnection sqlconn = new SqlConnection(mainconn);
 
             sqlconn.Open();
 
-            //begin
             string sqlquery1 =
                 "SELECT tbl_dailyReport.dailyReportID, tbl_Jobs.active, tbl_dailyReport.date, tbl_dailyReport.jobID, tbl_dailyReport.subJobID, " +
                 "tbl_subJobTypes.description, tbl_customers.customerName, tbl_customers.address, tbl_customers.customerCode, tbl_jobs.customerContact " +
                 "FROM tbl_dailyReport " +
 
-                    "INNER JOIN " +
-                    "tbl_Jobs ON tbl_Jobs.jobID = tbl_dailyReport.jobID " +
-                    "INNER JOIN " +
-                    "tbl_customers ON tbl_customers.customerCode = tbl_Jobs.customerCode " +
-                    "INNER JOIN " +
-                    "tbl_jobSubJobs ON tbl_jobSubJobs.jobID = tbl_Jobs.jobID " +
-                    "INNER JOIN " +
-                    "tbl_subJobTypes ON tbl_subJobTypes.subJobID = tbl_jobSubJobs.subJobID " +
-                    "WHERE " +
-                    "tbl_dailyReport.subJobID = tbl_subJobTypes.subJobID";
+                "INNER JOIN " +
+                "tbl_Jobs ON tbl_Jobs.jobID = tbl_dailyReport.jobID " +
+                "INNER JOIN " +
+                "tbl_customers ON tbl_customers.customerCode = tbl_Jobs.customerCode " +
+                "INNER JOIN " +
+                "tbl_jobSubJobs ON tbl_jobSubJobs.jobID = tbl_Jobs.jobID " +
+                "INNER JOIN " +
+                "tbl_subJobTypes ON tbl_subJobTypes.subJobID = tbl_jobSubJobs.subJobID " +
+                "WHERE " +
+                "tbl_dailyReport.subJobID = tbl_subJobTypes.subJobID";
 
             SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
-            //sqlcomm1.Parameters.AddWithValue("@dailyReportID", dailyReportID);
             SqlDataAdapter sda1 = new SqlDataAdapter(sqlcomm1);
             DataTable dt1 = new DataTable();
             sda1.Fill(dt1);
@@ -69,44 +65,17 @@ namespace allpax_service_record.Controllers
                 dailyRptViewAlls.Add(dailyRptViewAll);
             }
             sqlconn.Close();
-
             return View(dailyRptViewAlls);
         }
 
         public ActionResult Filtered(string startDate, string endDate)
         {
-
-            //string sql = @"SELECT tbl_dailyReport.dailyReportID, tbl_Jobs.active, tbl_dailyReport.date, tbl_dailyReport.jobID, " +
-            //"tbl_subJobTypes.description, tbl_customers.customerName, tbl_customers.address, tbl_customers.customerCode, tbl_jobs.customerContact, tbl_dailyReport.subJobID " +
-            //"FROM tbl_dailyReport " +
-
-            //"INNER JOIN " +
-            //"tbl_Jobs ON tbl_Jobs.jobID = tbl_dailyReport.jobID " +
-            //"INNER JOIN " +
-            //"tbl_customers ON tbl_customers.customerCode = tbl_Jobs.customerCode " +
-            //"INNER JOIN " +
-            //"tbl_jobSubJobs ON tbl_jobSubJobs.jobID = tbl_Jobs.jobID " +
-            //"INNER JOIN " +
-            //"tbl_subJobTypes ON tbl_subJobTypes.subJobID = tbl_jobSubJobs.subJobID " +
-            //"WHERE " +
-            //"tbl_dailyReport.subJobID = tbl_subJobTypes.subJobID " +
-            //"AND " +
-            //"tbl_dailyReport.date >= {0} " +
-            //"AND " +
-            //"tbl_dailyReport.date <= {1}";
-
-            //List<vm_dailyReportViewAll> list = db.Database.SqlQuery<vm_dailyReportViewAll>
-            //(sql, startDate, endDate).ToList();
-
-            //return View(list);
-
             List<vm_dailyReportViewAll> dailyRptViewAlls = new List<vm_dailyReportViewAll>();
             string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
             SqlConnection sqlconn = new SqlConnection(mainconn);
 
             sqlconn.Open();
 
-            //begin
             string sqlquery1 =
              "SELECT tbl_dailyReport.dailyReportID, tbl_Jobs.active, tbl_dailyReport.date, tbl_dailyReport.jobID, " +
             "tbl_subJobTypes.description, tbl_customers.customerName, tbl_customers.address, tbl_customers.customerCode, tbl_jobs.customerContact, tbl_dailyReport.subJobID " +
@@ -123,10 +92,8 @@ namespace allpax_service_record.Controllers
             "WHERE " +
             "tbl_dailyReport.subJobID = tbl_subJobTypes.subJobID " +
             "AND " +
-            //"tbl_dailyReport.date >= {0} " +
             "tbl_dailyReport.date >= @startDate " +
             "AND " +
-            //"tbl_dailyReport.date <= {1}";
             "tbl_dailyReport.date <= @endDate";
 
             SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
@@ -165,17 +132,13 @@ namespace allpax_service_record.Controllers
             string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
             SqlConnection sqlconn = new SqlConnection(mainconn);
 
-            //begin query for kits available but not installed by machine
             string sqlquery1 = "SELECT tbl_Users.name " +
             "FROM " +
             "tbl_Users " +
-
             "INNER JOIN " +
             "tbl_dailyReportUsers ON tbl_dailyReportUsers.userName = tbl_Users.userName " +
-
             "WHERE " +
             "tbl_dailyReportUsers.dailyReportID = @dailyReportID";
-            //end query for kits available but not installed by machine
 
             SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
             sqlcomm1.Parameters.Add(new SqlParameter("dailyReportID", dailyReportID));
