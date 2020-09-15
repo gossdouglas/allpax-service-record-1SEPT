9b687a79ef652e3fe652066ea71f0627f3d4aacf
gossdouglas <gossdouglas@yahoo.com>
2020-09-15 13:29:04 -0500

    finished the work.

--- Controllers/workDescController.cs
+++ Controllers/workDescController.cs
@@ -18,7 +18,70 @@ namespace allpax_service_record.Controllers
     {
         private allpaxServiceRecordEntities db = new allpaxServiceRecordEntities();
 
-        // GET: workDesc
+        //GET THE USER NAMES ATTACHED TO A TIME ENTRY
+        public List<string> workDescUsersByTimeEntryID(int timeEntryID)
+        {
+            List<string> userNames = new List<string>();
+
+            string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+
+            //begin query for kits available but not installed by machine
+            string sqlquery1 = "SELECT tbl_dailyReportTimeEntryUsers.userName " +
+
+            "FROM " +
+            "tbl_dailyReportTimeEntryUsers " +
+            "WHERE " +
+            "tbl_dailyReportTimeEntryUsers.timeEntryID = @timeEntryID";
+            //end query for kits available but not installed by machine
+
+            SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
+            sqlcomm1.Parameters.Add(new SqlParameter("timeEntryID", timeEntryID));
+            SqlDataAdapter sda3 = new SqlDataAdapter(sqlcomm1);
+            DataTable dt1 = new DataTable();
+            sda3.Fill(dt1);
+            foreach (DataRow dr1 in dt1.Rows)
+            {
+                userNames.Add(dr1[0].ToString());
+            }
+            return userNames;
+        }
+
+        //GET THE USER SHORT NAMES ATTACHED TO A TIME ENTRY
+        public List<string> workDescUserShortNamesByTimeEntryID(int timeEntryID)
+        {
+            List<string> userShortNames = new List<string>();
+
+            string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
+            SqlConnection sqlconn = new SqlConnection(mainconn);
+
+            //begin query for kits available but not installed by machine
+            string sqlquery1 = "SELECT tbl_Users.shortName " +
+
+            "FROM " +
+            "tbl_Users " +
+
+            "INNER JOIN " +
+            "tbl_dailyReportTimeEntryUsers ON " +
+            "tbl_Users.userName = tbl_dailyReportTimeEntryUsers.userName " +
+
+            "WHERE " +
+            "tbl_dailyReportTimeEntryUsers.timeEntryID = @timeEntryID";
+            //end query for kits available but not installed by machine
+
+            SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
+            sqlcomm1.Parameters.Add(new SqlParameter("timeEntryID", timeEntryID));
+            SqlDataAdapter sda1 = new SqlDataAdapter(sqlcomm1);
+            DataTable dt1 = new DataTable();
+            sda1.Fill(dt1);
+            foreach (DataRow dr1 in dt1.Rows)
+            {
+                userShortNames.Add(dr1[0].ToString());
+            }
+            return userShortNames;
+        }
+
+        //RETURN RECORDS FROM THE DATABASE
         public ActionResult Index(int dailyReportID)
          {
             List<vm_workDesc> workDescs = new List<vm_workDesc>();
@@ -61,117 +124,7 @@ namespace allpax_service_record.Controllers
             return View(workDescs);
         }
 
-        public List<string> workDescUsersByTimeEntryID(int timeEntryID)
-        {
-            List<string> userNames = new List<string>();
-
-            string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
-            SqlConnection sqlconn = new SqlConnection(mainconn);
-
-            //begin query for kits available but not installed by machine
-            string sqlquery1 = "SELECT tbl_dailyReportTimeEntryUsers.userName " +
-
-            "FROM " +
-            "tbl_dailyReportTimeEntryUsers " +
-            "WHERE " +
-            "tbl_dailyReportTimeEntryUsers.timeEntryID = @timeEntryID";
-            //end query for kits available but not installed by machine
-
-            SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
-            sqlcomm1.Parameters.Add(new SqlParameter("timeEntryID", timeEntryID));           
-            SqlDataAdapter sda3 = new SqlDataAdapter(sqlcomm1);
-            DataTable dt1 = new DataTable();
-            sda3.Fill(dt1);
-            foreach (DataRow dr1 in dt1.Rows)
-            {
-                userNames.Add(dr1[0].ToString());
-            }
-            return userNames;
-        }
-        public List<string> workDescUserShortNamesByTimeEntryID(int timeEntryID)
-        {
-            List<string> userShortNames = new List<string>();
-
-            string mainconn = ConfigurationManager.ConnectionStrings["allpaxServiceRecordEntities"].ConnectionString;
-            SqlConnection sqlconn = new SqlConnection(mainconn);
-
-            //begin query for kits available but not installed by machine
-            string sqlquery1 = "SELECT tbl_Users.shortName " +
-
-            "FROM " +
-            "tbl_Users " +
-
-            "INNER JOIN " +
-            "tbl_dailyReportTimeEntryUsers ON " +
-            "tbl_Users.userName = tbl_dailyReportTimeEntryUsers.userName " +
-
-            "WHERE " +
-            "tbl_dailyReportTimeEntryUsers.timeEntryID = @timeEntryID";
-            //end query for kits available but not installed by machine
-
-            SqlCommand sqlcomm1 = new SqlCommand(sqlquery1, sqlconn);
-            sqlcomm1.Parameters.Add(new SqlParameter("timeEntryID", timeEntryID));
-            SqlDataAdapter sda1 = new SqlDataAdapter(sqlcomm1);
-            DataTable dt1 = new DataTable();
-            sda1.Fill(dt1);
-            foreach (DataRow dr1 in dt1.Rows)
-            {
-                userShortNames.Add(dr1[0].ToString());
-            }
-            return userShortNames;
-        }
-     
-        [HttpPost]
-        public ActionResult AddWorkDesc(vm_workDesc workDescAdd)
-            {
-                ////--IF THE DAILY REPORT DOESN'T ALREADY EXIST...
-                //db.Database.ExecuteSqlCommand
-                //    ("DECLARE @id INT " +
-                //    "DECLARE @timeEntryID INT " +
-                //    "IF NOT EXISTS (SELECT * FROM tbl_dailyReportTimeEntry " +
-                //    "WHERE " +
-                //    "dailyReportID = {0} " +
-                //    "AND " +
-                //    "workDescription = {1} " +
-                //    "AND " +
-                //    "workDescriptionCategory ={3}) " +
-
-                //    "BEGIN " +
-
-                //    "INSERT INTO tbl_dailyReportTimeEntry VALUES({0}, {1}, {3}, {4}) " +
-                //    "SET @id = SCOPE_IDENTITY() " +
-                //    "INSERT INTO tbl_dailyReportTimeEntryUsers(timeEntryID, userName) VALUES(@id, {2}) " +
-
-                //    "END " +
-
-                //    //--IF THE DAILY REPORT DOES ALREADY EXIST...
-                //    "ELSE " +
-                //    "BEGIN " +
-
-                //    "SET @timeEntryID = " +
-                //        "(SELECT tbl_dailyReportTimeEntry.timeEntryID " +
-                //        "FROM tbl_dailyReportTimeEntry " +
-                //        "WHERE " +
-
-                //        "tbl_dailyReportTimeEntry.dailyReportID like {0} " +
-                //        "AND " +
-                //        "workDescription = {1} " +
-                //        "AND " +
-                //        "workDescriptionCategory = {3}) " +
-
-                //        "INSERT INTO tbl_dailyReportTimeEntryUsers(timeEntryID, userName) VALUES(@timeEntryID, {02}) " +
-
-                //    "END",
-
-                //    workDescAdd.dailyReportID, workDescAdd.workDescription, workDescAdd.userName, workDescAdd.workDescriptionCategory, workDescAdd.hours);
-           
-                return new EmptyResult();
-                ////return View();
-                ////return RedirectToAction("Index");
-                ////return RedirectToAction("Index", new { dailyReportID = workDescAdd.dailyReportID });
-                ////return RedirectToAction("Index", "DailyReportByReportID", new { reportID = workDescAdd.dailyReportID});
-            }
-
+        //ADD A NARRATIVE TO A NEW DELAYS RECORD
         [HttpPost]
         public ActionResult AddWorkDescNarr(vm_workDesc workDescAdd)
          {
@@ -185,6 +138,7 @@ namespace allpax_service_record.Controllers
                 return new EmptyResult();
         }
 
+        //ADD A TEAM MEMBER TO A NEW DELAYS RECORD
         [HttpPost]
         public ActionResult AddWorkDescTeam(vm_workDesc workDescAdd)
         {
@@ -211,6 +165,7 @@ namespace allpax_service_record.Controllers
             return new EmptyResult();
         }
 
+        //ADD A TEAM MEMBER TO A TIME ENTRY
         [HttpPost]
         public ActionResult AddTeamMember(vm_workDesc teamMemberAdd)
         {
@@ -220,6 +175,7 @@ namespace allpax_service_record.Controllers
             return new EmptyResult();
         }
 
+        //DELETE A TIME ENTRY
         public ActionResult DeleteWorkDesc(vm_workDesc workDescDelete)
         {
             db.Database.ExecuteSqlCommand("DELETE FROM tbl_dailyReportTimeEntry WHERE timeEntryID=({0})", workDescDelete.timeEntryID);
@@ -227,6 +183,7 @@ namespace allpax_service_record.Controllers
             return new EmptyResult();
         }
 
+        //DELETE A TEAM MEMBER FROM A TIME ENTRY
         [HttpPost]
         public ActionResult DeleteTeamMember(vm_workDesc teamMemberDelete)
         {
@@ -239,6 +196,7 @@ namespace allpax_service_record.Controllers
             return new EmptyResult();
         }
 
+        //UPDATE A TIME ENTRY
         public ActionResult UpdateWorkDesc(vm_workDesc workDescUpdate)
         {
             db.Database.ExecuteSqlCommand("UPDATE tbl_dailyReportTimeEntry SET workDescription={1} WHERE timeEntryID={0}",
@@ -246,7 +204,6 @@ namespace allpax_service_record.Controllers
 
             return new EmptyResult();
         }
-
 
         protected override void Dispose(bool disposing)
         {
