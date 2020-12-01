f83baca4a3269e68bc84773a3ec19f48dc9f678b
gossdouglas <gossdouglas@yahoo.com>
2020-12-01 15:28:00 -0600

    not all changes stuck.

--- Models/View Models/vm_userAcctInfo.cs
+++ Models/View Models/vm_userAcctInfo.cs
@@ -9,11 +9,36 @@ namespace allpax_service_record.Models.View_Models
     public partial class vm_userAcctInfo
     {
         public string aspNetId { get; set; }
+
+        //public string UserName { get; set; }
+        [Required]
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
+
+        //public string name { get; set; }
+        [Required]
+        [Display(Name = "Full Name")]
         public string name { get; set; }
+
+        //public string ShortName { get; set; }
+        [Required]
+        [Display(Name = "Short Name")]
         public string ShortName { get; set; }
+
+        //public bool admin { get; set; }
+        [Required]
+        [Display(Name = "Admin Status")]
         public bool admin { get; set; }
+
+        //public bool active { get; set; }
+        [Required]
+        [Display(Name = "Active")]
         public bool active { get; set; }
+
+        //public string Email { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
     }
 }
