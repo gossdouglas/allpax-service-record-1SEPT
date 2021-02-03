08cd8b633e8081df4226658273770f134c10dddb
gossdouglas <gossdouglas@yahoo.com>
2021-02-03 14:41:52 -0600

    fixed the problem.

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -492,7 +492,6 @@ namespace allpax_service_record.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            //return RedirectToAction("Index", "Home");
             return RedirectToAction("Login", "Account");
         }
 
